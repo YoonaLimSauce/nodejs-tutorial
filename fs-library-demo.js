04d1a5514f97609ad464fa2676cc0a018548cb6f
Yoona Lim <miraclefishleong@gmail.com>
2024-04-12 22:21:32 +0800

    path模块的使用

--- fs-library-demo.js
+++ fs-library-demo.js
@@ -2,7 +2,7 @@
  * @Author: Yoona Lim miraclefishleong@gmail.com
  * @Date: 2024-04-12 19:20:49
  * @LastEditors: Yoona Lim miraclefishleong@gmail.com
- * @LastEditTime: 2024-04-12 21:42:57
+ * @LastEditTime: 2024-04-12 22:19:19
  * @FilePath: \Git\node-js-demo\fs-library-demo.js
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
@@ -10,6 +10,20 @@
 
 const fs = require('fs')
 const path = require('path')
+
+printsegment()
+
+console.log('path模块示例：')
+console.log('Current directory:\t\t', __dirname)
+const testPath = path.join(__dirname, 'fs-library-test.txt')
+console.log('Current file path:\t\t', testPath)
+console.log('File name:\t\t\t', path.basename(testPath))
+console.log('File extension:\t\t\t', path.basename(testPath, '.txt'))
+console.log('Directory name:\t\t\t', path.basename(testPath, path.extname(testPath)))
+
+printsegment()
+
+console.log('fs模块示例：')
 
 // Read file content
 fs.readFile('fs-library-test.txt', 'utf8', (err, data) => {
@@ -20,9 +34,6 @@ fs.readFile('fs-library-test.txt', 'utf8', (err, data) => {
   console.log(data)
 })
 
-console.log('Current directory:', __dirname)
-console.log('Current file path:', path.join(__dirname, 'fs-library-test.txt'))
-
 // Write file content
 fs.writeFile(__dirname +'fs-library-test.txt', 'Hello Node.js!', (err) => {
   if (err) {
@@ -31,3 +42,7 @@ fs.writeFile(__dirname +'fs-library-test.txt', 'Hello Node.js!', (err) => {
   }
   console.log('File written successfully!')
 })
+
+function printsegment() {
+  console.log('************************************************************************************************************************')
+}
