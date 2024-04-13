7592bf68d6367b6c21b8a57e286fb54d4cc6ce5b
Yoona Lim <miraclefishleong@gmail.com>
2024-04-13 11:42:11 +0800

    使用node-js创建一个监听8080端口的web服务器，网络浏览器使用request响应请求信息，response响应服务器返回信息

--- http-library-demo.js
+++ http-library-demo.js
@@ -2,7 +2,7 @@
  * @Author: Yoona Lim miraclefishleong@gmail.com
  * @Date: 2024-04-13 08:18:42
  * @LastEditors: Yoona Lim miraclefishleong@gmail.com
- * @LastEditTime: 2024-04-13 08:19:02
+ * @LastEditTime: 2024-04-13 11:38:42
  * @FilePath: \Git\node-js-demo\http-library-demo.js
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
@@ -13,7 +13,14 @@ const http = require('http')
 const myServer = http.createServer()
 
 myServer.on('request', (req, res) => {
-  console.log('request received')
+//   console.log('request received')
+    const url = req.url
+    const method = req.method
+
+    const responseString = `Your request url is ${url}, and request method is ${method}`
+    console.log(responseString)
+
+    res.end(responseString)
 })
 
 myServer.listen(8080, () => {
