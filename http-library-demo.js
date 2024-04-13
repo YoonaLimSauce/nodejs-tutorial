f48d4654faa970867c21885dce96599dbb029b21
Yoona Lim <miraclefishleong@gmail.com>
2024-04-13 11:46:44 +0800

    使用node-js创建一个监听8080端口的web服务器，网络浏览器使用request响应请求信息，response响应服务器返回信息，处理返回中文response信息乱码问题

--- http-library-demo.js
+++ http-library-demo.js
@@ -2,7 +2,7 @@
  * @Author: Yoona Lim miraclefishleong@gmail.com
  * @Date: 2024-04-13 08:18:42
  * @LastEditors: Yoona Lim miraclefishleong@gmail.com
- * @LastEditTime: 2024-04-13 11:38:42
+ * @LastEditTime: 2024-04-13 11:44:44
  * @FilePath: \Git\node-js-demo\http-library-demo.js
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
@@ -20,7 +20,9 @@ myServer.on('request', (req, res) => {
     const responseString = `Your request url is ${url}, and request method is ${method}`
     console.log(responseString)
 
-    res.end(responseString)
+    res.setHeader('Content-Type', 'text/html; charset=utf-8')
+    const responseChineseString = `你的请求头信息是 ${url}, and 请求方式是 ${method}`
+    res.end(responseChineseString)
 })
 
 myServer.listen(8080, () => {
