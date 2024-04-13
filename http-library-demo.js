c8736d252d281850466d141a6a8dfe7e527cd1d8
Yoona Lim <miraclefishleong@gmail.com>
2024-04-13 08:24:08 +0800

    使用node-js提供的http模块可以自建网络web服务器

---
+++ http-library-demo.js
@@ -0,0 +1,12 @@
+/*
+ * @Author: Yoona Lim miraclefishleong@gmail.com
+ * @Date: 2024-04-13 08:18:42
+ * @LastEditors: Yoona Lim miraclefishleong@gmail.com
+ * @LastEditTime: 2024-04-13 08:19:02
+ * @FilePath: \Git\node-js-demo\http-library-demo.js
+ * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+ */
+'use strict'
+
+const http = require('http')
+
