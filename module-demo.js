9ac30e08dd1b1ff8d34850d129444a303ff48765
Yoona Lim <miraclefishleong@gmail.com>
2024-04-14 14:06:51 +0800

    使用module关键字可以获取当前module模块的信息，并将module信息进行打印

---
+++ module-demo.js
@@ -0,0 +1,12 @@
+/*
+ * @Author: Yoona Lim miraclefishleong@gmail.com
+ * @Date: 2024-04-14 13:53:28
+ * @LastEditors: Yoona Lim miraclefishleong@gmail.com
+ * @LastEditTime: 2024-04-14 14:04:52
+ * @FilePath: \Git\node-js-demo\module-demo.js
+ * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+ */
+'use strict'
+
+console.log('演示打印module模块信息：')
+console.log(module)
