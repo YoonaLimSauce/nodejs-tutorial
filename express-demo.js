538149ad6d6cbac99e4cb1fac0df50d2e261aa64
Yoona Lim <miraclefishleong@gmail.com>
2024-04-16 21:09:43 +0800

    使用express创建一个服务器对象app，app设置相应get、post请求并监听80端口

--- express-demo.js
+++ express-demo.js
@@ -2,7 +2,7 @@
  * @Author: Yoona Lim miraclefishleong@gmail.com
  * @Date: 2024-04-16 20:49:30
  * @LastEditors: Yoona Lim miraclefishleong@gmail.com
- * @LastEditTime: 2024-04-16 21:00:00
+ * @LastEditTime: 2024-04-16 21:07:51
  * @FilePath: \node-js-demo\express-demo.js
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
@@ -11,6 +11,13 @@ const express = require('express');
 
 // 创建 Web 服务器
 const app = express();
+
+app.get('/user', (req, res) => {
+    res.send({ name: '张三', age: 20, gender: '男' });
+});
+app.post('/user', (req, res) => {
+    res.send('用户信息保存成功');
+});
 
 // 启动 Web 服务器 监听端口
 app.listen(80, () => {
