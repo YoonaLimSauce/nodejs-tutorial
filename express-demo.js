88ca7631d29d639eeb6ce4d805c2c0151aae97cf
Yoona Lim <miraclefishleong@gmail.com>
2024-04-16 21:15:46 +0800

    express搭建服务器，使用回调函数中request参数，request对象中query属性包含了请求头信息

--- express-demo.js
+++ express-demo.js
@@ -2,7 +2,7 @@
  * @Author: Yoona Lim miraclefishleong@gmail.com
  * @Date: 2024-04-16 20:49:30
  * @LastEditors: Yoona Lim miraclefishleong@gmail.com
- * @LastEditTime: 2024-04-16 21:07:51
+ * @LastEditTime: 2024-04-16 21:14:29
  * @FilePath: \node-js-demo\express-demo.js
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
@@ -18,6 +18,10 @@ app.get('/user', (req, res) => {
 app.post('/user', (req, res) => {
     res.send('用户信息保存成功');
 });
+app.get('/', (req, res) => {
+    console.log(req.query)
+    res.send(req.query)
+})
 
 // 启动 Web 服务器 监听端口
 app.listen(80, () => {
