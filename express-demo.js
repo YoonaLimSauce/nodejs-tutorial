e43af1c71faaf8add1f2dc33097a6456808b9167
Yoona Lim <miraclefishleong@gmail.com>
2024-04-16 22:15:12 +0800

    express搭建服务器，使用回调函数中request参数，request对象中params属性包含了动态参数值

--- express-demo.js
+++ express-demo.js
@@ -2,7 +2,7 @@
  * @Author: Yoona Lim miraclefishleong@gmail.com
  * @Date: 2024-04-16 20:49:30
  * @LastEditors: Yoona Lim miraclefishleong@gmail.com
- * @LastEditTime: 2024-04-16 21:14:29
+ * @LastEditTime: 2024-04-16 22:13:09
  * @FilePath: \node-js-demo\express-demo.js
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
@@ -21,6 +21,10 @@ app.post('/user', (req, res) => {
 app.get('/', (req, res) => {
     console.log(req.query)
     res.send(req.query)
+})
+app.get('/user/:id', (req, res) => {
+    console.log(req.params)
+    res.send(req.params)
 })
 
 // 启动 Web 服务器 监听端口
