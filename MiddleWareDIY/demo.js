28de7147f3b7a4f380611a73eaeebcddb95560f6
Yoona Lim <miraclefishleong@gmail.com>
2024-04-20 07:52:03 +0800

    通过引入中间件cors解决跨域问题

--- MiddleWareDIY/demo.js
+++ MiddleWareDIY/demo.js
@@ -2,12 +2,19 @@
  * @Author: Yoona Lim miraclefishleong@gmail.com
  * @Date: 2024-04-20 06:54:10
  * @LastEditors: Yoona Lim miraclefishleong@gmail.com
- * @LastEditTime: 2024-04-20 06:55:46
+ * @LastEditTime: 2024-04-20 07:49:39
  * @FilePath: \node-js-demo\MiddleWareDIY\demo.js
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
 const express = require('express');
 const app = express();
+
+app.get('/api/jsonp', (req, res) => {
+    console.log('hello world')
+});
+
+const cors = require('cors')
+app.use(cors())
 
 app.use(express.urlencoded({ extended: false }));
 const router = require('./middleware');
