1646d4854e8db7797972f456fc60b2fc364a88dd
Yoona Lim <miraclefishleong@gmail.com>
2024-04-21 23:13:40 +0800

    使用express-session模块导入中间件session并配置到服务器中，将session信息保存在服务器中，根据session判断客户端登录状态并响应不同信息

--- session-cookie-demo.js
+++ session-cookie-demo.js
@@ -2,7 +2,7 @@
  * @Author: Yoona Lim miraclefishleong@gmail.com
  * @Date: 2024-04-21 22:40:58
  * @LastEditors: Yoona Lim miraclefishleong@gmail.com
- * @LastEditTime: 2024-04-21 22:49:05
+ * @LastEditTime: 2024-04-21 23:09:39
  * @FilePath: \node-js-demo\session-cookie-demo.js
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
@@ -26,6 +26,14 @@ app.post('/api/login', (req, res) => {
   req.session.isLogin = true
 
   res.send({ status: 0, msg: '登录成功' })
+})
+
+app.get('/api/username', (req, res) => {
+  if (!req.session.isLogin) {
+    return res.send({ status: 1, msg: '请先登录' })
+  }
+
+  res.send({ status: 0, msg: '获取成功', data: req.session.username })
 })
 
 app.listen(3000, () => {
