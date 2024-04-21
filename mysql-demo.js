58c2e46b8f0ed7e32df7ff72050d230509c39359
Yoona Lim <miraclefishleong@gmail.com>
2024-04-21 09:45:50 +0800

    使用mysql模块的query语法对mysql数据库进行crud增删改查操作

--- mysql-demo.js
+++ mysql-demo.js
@@ -2,7 +2,7 @@
  * @Author: Yoona Lim miraclefishleong@gmail.com
  * @Date: 2024-04-21 09:12:13
  * @LastEditors: Yoona Lim miraclefishleong@gmail.com
- * @LastEditTime: 2024-04-21 09:13:40
+ * @LastEditTime: 2024-04-21 09:17:33
  * @FilePath: \node-js-demo\mysql-demo.js
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
@@ -13,4 +13,12 @@ const database = mysql.createPool({
   user: 'root',
   password: '123456',
   database: 'mydatabase'
+})
+
+database.query('SELECT * FROM mydatatable', (err, results) => {
+  if (err) {
+    return console.log(err.message)
+  } else {
+    console.log(results)
+  }
 })
