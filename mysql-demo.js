b0b893748604c6f00d00fd57cd332a9690c371b3
Yoona Lim <miraclefishleong@gmail.com>
2024-04-21 10:52:28 +0800

    使用mysql模块的query方法配合模板字符串实现更新数据

--- mysql-demo.js
+++ mysql-demo.js
@@ -2,7 +2,7 @@
  * @Author: Yoona Lim miraclefishleong@gmail.com
  * @Date: 2024-04-21 09:12:13
  * @LastEditors: Yoona Lim miraclefishleong@gmail.com
- * @LastEditTime: 2024-04-21 10:25:16
+ * @LastEditTime: 2024-04-21 10:47:38
  * @FilePath: \node-js-demo\mysql-demo.js
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
@@ -49,5 +49,16 @@ database.query(sqlStr2, user2, (err, result) => {
   }
   if (result.affectedRows > 0) {
     console.log('Insert success')
+  }
+})
+
+const user3 = { id: 9, username: 'Spider-Man2', password: '小爱同学' }
+const sqlStr3 = 'UPDATE mydatatable SET username = ?, password = ? WHERE id = ?'
+database.query(sqlStr3, [user3.username, user3.password, user3.id], (err, result) => {
+  if (err) {
+    return console.log(err.message)
+  }
+  if (result.affectedRows > 0) {
+    console.log('Update success')
   }
 })
