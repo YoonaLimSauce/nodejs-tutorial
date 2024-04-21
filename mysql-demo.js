2867e3d55aa7a528047a20b6c078c9fe1694184d
Yoona Lim <miraclefishleong@gmail.com>
2024-04-21 10:26:35 +0800

    使用mysql模块的query方法实现便捷插入新数据

--- mysql-demo.js
+++ mysql-demo.js
@@ -2,7 +2,7 @@
  * @Author: Yoona Lim miraclefishleong@gmail.com
  * @Date: 2024-04-21 09:12:13
  * @LastEditors: Yoona Lim miraclefishleong@gmail.com
- * @LastEditTime: 2024-04-21 10:10:42
+ * @LastEditTime: 2024-04-21 10:25:16
  * @FilePath: \node-js-demo\mysql-demo.js
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
@@ -38,5 +38,16 @@ database.query('SELECT * FROM mydatatable', (err, result) => {
     return console.log(err.message)
   } else {
     console.log('插入成功后的数据:', result)
+  }
+})
+
+const user2 = { username: 'Spider-Man2', password: 'pcc4321' }
+const sqlStr2 = 'INSERT INTO mydatatable SET ?'
+database.query(sqlStr2, user2, (err, result) => {
+  if (err) {
+    return console.log(err.message)
+  }
+  if (result.affectedRows > 0) {
+    console.log('Insert success')
   }
 })
