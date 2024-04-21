585970746aeaab2db82a809a3c4d158f241dd118
Yoona Lim <miraclefishleong@gmail.com>
2024-04-22 00:01:57 +0800

    使用jwtwebtoken模块生成token并在response中发送token信息

--- session-cookie-demo.js
+++ session-cookie-demo.js
@@ -2,7 +2,7 @@
  * @Author: Yoona Lim miraclefishleong@gmail.com
  * @Date: 2024-04-21 22:40:58
  * @LastEditors: Yoona Lim miraclefishleong@gmail.com
- * @LastEditTime: 2024-04-21 23:51:50
+ * @LastEditTime: 2024-04-21 23:58:27
  * @FilePath: \node-js-demo\session-cookie-demo.js
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
@@ -22,6 +22,8 @@ app.use(session({
 
 app.use(express.urlencoded({ extended: false }))
 
+const secretKey = 'secretKey'
+
 app.post('/api/login', (req, res) => {
   if (req.body.username !== 'admin' || req.body.password !== '123456') {
     return res.send({ status: 1, msg: '登录失败' })
@@ -30,7 +32,9 @@ app.post('/api/login', (req, res) => {
   req.session.user = req.body
   req.session.isLogin = true
 
-  res.send({ status: 0, msg: '登录成功' })
+  const token = jwt.sign({ username: req.session.user.username }, secretKey, { expiresIn: '30s' })
+
+  res.send({ status: 0, msg: '登录成功', token: token })
 })
 
 app.get('/api/username', (req, res) => {
