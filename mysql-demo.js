df834c4aa509c910b2b37fe57b58bfa929352a90
Yoona Lim <miraclefishleong@gmail.com>
2024-04-21 10:59:03 +0800

    使用mysql模块的query方法实现根据主键删除数据

--- mysql-demo.js
+++ mysql-demo.js
@@ -2,7 +2,7 @@
  * @Author: Yoona Lim miraclefishleong@gmail.com
  * @Date: 2024-04-21 09:12:13
  * @LastEditors: Yoona Lim miraclefishleong@gmail.com
- * @LastEditTime: 2024-04-21 10:47:38
+ * @LastEditTime: 2024-04-21 10:56:39
  * @FilePath: \node-js-demo\mysql-demo.js
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
@@ -60,5 +60,26 @@ database.query(sqlStr3, [user3.username, user3.password, user3.id], (err, result
   }
   if (result.affectedRows > 0) {
     console.log('Update success')
+  }
+})
+
+const user4 = { id: 6, username: 'Spider-Man', password: '小爱同学，Are you OK?' }
+const sqlStr4 = 'UPDATE mydatatable SET ?  WHERE id = ?'
+database.query(sqlStr4, [user4, user4.id], (err, result) => {
+  if (err) {
+    return console.log(err.message)
+  }
+  if (result.affectedRows > 0) {
+    console.log('Update success')
+  }
+})
+
+const sqlStr5 = 'DELETE FROM mydatatable WHERE id = ?'
+database.query(sqlStr5, 8, (err, result) => {
+  if (err) {
+    return console.log(err.message)
+  }
+  if (result.affectedRows > 0) {
+    console.log('Delete success')
   }
 })
