0e7c3e8845d3b011e787f1c4a38271f5cdbc557f
Yoona Lim <miraclefishleong@gmail.com>
2024-04-22 00:05:22 +0800

    使用express-jwt模块将jwt字符串还原为json字符串，服务端通过解析后字符串判断客户端登录状态

--- session-cookie-demo.js
+++ session-cookie-demo.js
@@ -2,7 +2,7 @@
  * @Author: Yoona Lim miraclefishleong@gmail.com
  * @Date: 2024-04-21 22:40:58
  * @LastEditors: Yoona Lim miraclefishleong@gmail.com
- * @LastEditTime: 2024-04-21 23:58:27
+ * @LastEditTime: 2024-04-22 00:03:25
  * @FilePath: \node-js-demo\session-cookie-demo.js
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
@@ -23,6 +23,8 @@ app.use(session({
 app.use(express.urlencoded({ extended: false }))
 
 const secretKey = 'secretKey'
+
+app.use(expressJWT({ secret: secretKey }).unless({ path: ['/\/api\//'] }))
 
 app.post('/api/login', (req, res) => {
   if (req.body.username !== 'admin' || req.body.password !== '123456') {
