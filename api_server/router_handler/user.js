96f6140b5456f31dd5473c6034ee8b1c6736c661
Yoona Lim <miraclefishleong@gmail.com>
2024-04-29 22:35:39 +0800

    mysql查询数据库是否有重复元素

--- api_server/router_handler/user.js
+++ api_server/router_handler/user.js
@@ -1,3 +1,7 @@
+const db = require('../db/index')
+
+const sqlStr_user_existed = `select * from ev_users where username=?`
+
 module.exports.regUser = (req, res) => {
     const userinfo = req.body
 
@@ -5,7 +9,14 @@ module.exports.regUser = (req, res) => {
         return res.send({ status: 1, msg: '用户名和密码不能为空' })
     }
 
-    res.send('请求响应函数已分离，reguser OK')
+    db.query(sqlStr_user_existed, userinfo.username, (err, results) => {
+        if (err) {
+            return res.send({ status: 1, msg: err.message })
+        }
+        if (results.length > 0) {
+            return res.send({ status: 1, msg: '用户名已存在' })
+        }
+    })
 }
 
 module.exports.login = (req, res) => {
