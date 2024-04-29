fb7b28626574815fc63795246a9d822899d37b9d
Yoona Lim <miraclefishleong@gmail.com>
2024-04-30 00:07:13 +0800

    nodejs服务器，响应mysql异步编程

--- api_server/router_handler/user.js
+++ api_server/router_handler/user.js
@@ -3,26 +3,42 @@ const db = require('../db/index')
 const bcrypt = require('bcryptjs')
 
 const sqlStr_user_existed = `select * from ev_users where username=?`
+const sqlStr_user_insert = `insert into ev_users set ?`
 
 module.exports.regUser = (req, res) => {
     const userinfo = req.body
 
     if (!userinfo.username || !userinfo.password) {
-        return res.send({ status: 1, msg: '用户名和密码不能为空' })
+        res.send({ status: 1, msg: '用户名和密码不能为空' })
+        return
     }
 
     db.query(sqlStr_user_existed, userinfo.username, (err, results) => {
         if (err) {
-            return res.send({ status: 1, msg: err.message })
+            res.send({ status: 1, msg: err.message })
+            return
         }
         if (results.length > 0) {
-            return res.send({ status: 1, msg: '用户名已存在' })
+            res.send({ status: 1, msg: '用户名已存在' })
+            return
         }
+
+        userinfo.password = bcrypt.hashSync(userinfo.password, 10)
+
+        db.query(sqlStr_user_insert, { username: userinfo.username, password: userinfo.password }, (err, results) => {
+            if (err) {
+                res.send({ status: 1, msg: err.message })
+                return
+            }
+            if (results.affectedRows !== 1) {
+                res.send({ status: 1, msg: '注册用户失败' })
+                return
+            }
+            res.send({ status: 0, msg: '注册用户成功' })
+            return
+        })
     })
 
-    console.log(userinfo)
-    userinfo.password = bcrypt.hashSync(userinfo.password, 10)
-    console.log(userinfo)
 }
 
 module.exports.login = (req, res) => {
