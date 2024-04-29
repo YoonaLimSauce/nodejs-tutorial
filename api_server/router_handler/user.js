45a2c3e08778a4cab48f31afc344922d2f65adf5
Yoona Lim <miraclefishleong@gmail.com>
2024-04-29 21:27:44 +0800

    检测登录表单数据是否合法

--- api_server/router_handler/user.js
+++ api_server/router_handler/user.js
@@ -1,4 +1,10 @@
 module.exports.regUser = (req, res) => {
+    const userinfo = req.body
+
+    if (!userinfo.username || !userinfo.password) {
+        return res.send({ status: 1, msg: '用户名和密码不能为空' })
+    }
+
     res.send('请求响应函数已分离，reguser OK')
 }
 
