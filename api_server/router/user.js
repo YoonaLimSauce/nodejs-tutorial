42fc3da29c7f180de12c852109d099c67398f6aa
Yoona Lim <miraclefishleong@gmail.com>
2024-04-30 21:50:44 +0800

    使用同样的方式joi验证登录数据

--- api_server/router/user.js
+++ api_server/router/user.js
@@ -2,7 +2,7 @@
  * @Author: Yoona Lim miraclefishleong@gmail.com
  * @Date: 2024-04-28 22:28:15
  * @LastEditors: Yoona Lim miraclefishleong@gmail.com
- * @LastEditTime: 2024-04-30 21:29:36
+ * @LastEditTime: 2024-04-30 21:49:15
  * @FilePath: \node-js-demo\api_server\router\user.js
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
@@ -15,6 +15,6 @@ const router = express.Router();
 
 router.post('/reguser', expressJoi(reg_login_schema), user_handler.regUser);
 
-router.post('/login', user_handler.login);
+router.post('/login', expressJoi(reg_login_schema), user_handler.login);
 
 module.exports = router;
