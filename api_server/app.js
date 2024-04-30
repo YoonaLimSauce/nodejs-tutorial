bef4c8dfe8d25f505ac7f72c8645322c21d80871
Yoona Lim <miraclefishleong@gmail.com>
2024-05-01 07:34:39 +0800

    使用express-jwt配置解析token的中间件

--- api_server/app.js
+++ api_server/app.js
@@ -2,7 +2,7 @@
  * @Author: Yoona Lim miraclefishleong@gmail.com
  * @Date: 2024-04-28 22:02:30
  * @LastEditors: Yoona Lim miraclefishleong@gmail.com
- * @LastEditTime: 2024-04-30 21:35:03
+ * @LastEditTime: 2024-05-01 07:31:59
  * @FilePath: \node-js-demo\api_server\app.js
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
@@ -10,6 +10,8 @@ const express = require('express')
 const cors = require('cors')
 const joi = require('joi')
 const userRouter = require('./router/user')
+const { expressjwt : expressJWT } = require('express-jwt')
+const config = require('./config')
 
 const app = express()
 app.use(cors())
@@ -23,11 +25,17 @@ app.use(function(req, res, next) {
     }
     next()
 })
+
+app.use(expressJWT({ secret: config.jwtsecretKey, algorithms: ['HS256'] }).unless({ path: [/^\/api\//] }))
+
 app.use('/api', userRouter)
 
 app.use(function(err, req, res, next) {
     if (err instanceof joi.ValidationError) {
         return res.cc(err)
+    }
+    if (err.name === 'UnauthorizedError') {
+        return res.cc('身份认证失败')
     }
     res.cc(err)
 })
