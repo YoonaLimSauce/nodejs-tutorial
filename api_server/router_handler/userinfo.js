0e46e3b8f7424d8d78fabcedbd544aeb7a604231
Yoona Lim <miraclefishleong@gmail.com>
2024-05-01 11:36:03 +0800

    根据token查询数据库中当前用户的信息

--- api_server/router_handler/userinfo.js
+++ api_server/router_handler/userinfo.js
@@ -1,3 +1,19 @@
+const db = require('../db/index');
+
+const sqlStr_userinfo_existed = `select id, username, nickname, email, user_pic from ev_users where id = ?`;
+
 module.exports.getUserInfo = (req, res) => {
-    res.send('UserInfo');
+    db.query(sqlStr_userinfo_existed, [req.auth.id], (err, results) => {
+        if (err) {
+            return res.cc(err);
+        }
+        if (results.length !== 1) {
+            return res.cc('获取用户信息失败');
+        }
+        res.send({
+            status: 0,
+            msg: '获取用户信息成功',
+            data: results[0]
+        });
+    });
 }
