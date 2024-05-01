07f46835f61e3e935ec91676097be24a323a31ab
Yoona Lim <miraclefishleong@gmail.com>
2024-05-01 14:45:30 +0800

    中间件验证输入的数据

--- api_server/router/userinfo.js
+++ api_server/router/userinfo.js
@@ -1,10 +1,12 @@
 const express = require('express');
+const expressJoi = require('@escook/express-joi');
+const { update_userinfo_schema } = require('../schema/user');
 const userinfo_handler = require('../router_handler/userinfo');
 
 const router = express.Router();
 
 router.get('/userinfo', userinfo_handler.getUserInfo);
 
-router.post('/userinfo', userinfo_handler.updateUserInfo);
+router.post('/userinfo', expressJoi(update_userinfo_schema), userinfo_handler.updateUserInfo);
 
 module.exports = router;
