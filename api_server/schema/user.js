ddc942bef78b7be3d1362b63db2eb064ee95b2b1
Yoona Lim <miraclefishleong@gmail.com>
2024-05-01 14:41:57 +0800

    定义验证规则

--- api_server/schema/user.js
+++ api_server/schema/user.js
@@ -3,9 +3,21 @@ const joi = require('joi')
 const username = joi.string().alphanum().min(1).max(10).required()
 const password = joi.string().pattern(/^[\S]{6,12}$/).required()
 
+const id = joi.number().integer().min(1).required()
+const nickname = joi.string().required()
+const email = joi.string().email().required()
+
 module.exports.reg_login_schema = {
   body: {
     username: username,
     password: password
+  }
+}
+
+module.exports.update_userinfo_schema = {
+  body: {
+    id: id,
+    nickname: nickname,
+    email: email
   }
 }
